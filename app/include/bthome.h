c6550054f13425af10630de604878073e06aea66
Jeff Rizzo <riz@tastylime.net>
2024-02-13 08:20:39 -0800

    Move BTHome defines into separate #include file

---
+++ app/include/bthome.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2024 Jeffrey C Rizzo
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+// BTHome device information, from https://bthome.io/format/
+#define BTHOME_ENCRYPTION_FLAG 0x01
+#define BTHOME_TRIGGER_BASED_FLAG 0x04 // irregular advertising interval
+#define BTHOME_VERSION_2 0x40
+
+// object IDs
+#define BTHOME_PACKET_ID 0x0
+#define BTHOME_BUTTON_EVENT 0x3a
+
+// the event types for the button event
+#define BTHOME_BUTTON_EVENT_NONE 0
+#define BTHOME_BUTTON_EVENT_PRESS 1
+#define BTHOME_BUTTON_EVENT_DOUBLE_PRESS 2
+#define BTHOME_BUTTON_EVENT_TRIPLE_PRESS 3
+#define BTHOME_BUTTON_EVENT_LONG_PRESS 4
+#define BTHOME_BUTTON_EVENT_LONG_DOUBLE_PRESS 5
+#define BTHOME_BUTTON_EVENT_LONG_TRIPLE_PRESS 6
+
+#define BTHOME_SERVICE_UUID 0xfcd2
+
--- app/src/main.c
+++ app/src/main.c
@@ -10,27 +10,8 @@
 
 #include <zephyr/logging/log.h>
 
+#include <bthome.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
-
-// BTHome device information, from https://bthome.io/format/
-#define BTHOME_ENCRYPTION_FLAG 0x01
-#define BTHOME_TRIGGER_BASED_FLAG 0x04 // irregular advertising interval
-#define BTHOME_VERSION_2 0x40
-
-// object IDs
-#define BTHOME_PACKET_ID 0x0
-#define BTHOME_BUTTON_EVENT 0x3a
-
-// the event types for the button event
-#define BTHOME_BUTTON_EVENT_NONE 0
-#define BTHOME_BUTTON_EVENT_PRESS 1
-#define BTHOME_BUTTON_EVENT_DOUBLE_PRESS 2
-#define BTHOME_BUTTON_EVENT_TRIPLE_PRESS 3
-#define BTHOME_BUTTON_EVENT_LONG_PRESS 4
-#define BTHOME_BUTTON_EVENT_LONG_DOUBLE_PRESS 5
-#define BTHOME_BUTTON_EVENT_LONG_TRIPLE_PRESS 6
-
-#define BTHOME_SERVICE_UUID 0xfcd2
 
 struct bt_le_adv_param adv_param = {
     .id = BT_ID_DEFAULT,
